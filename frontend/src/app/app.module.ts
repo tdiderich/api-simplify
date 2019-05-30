ce367b808c6e0fe21a3fa7c9033fcad8a94e0680
tdiderich <tyler.diderich@jask.io>
2019-05-29 18:16:44 -0700

    Integrating Angular with Auth0

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,44 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {ExamsApiService} from './exams/exams-api.service';
 
+import {ExamFormComponent} from './exams/exam-form.component';
+import {RouterModule, Routes} from '@angular/router';
+import {ExamsComponent} from './exams/exams.component';
+
+import * as Auth0 from 'auth0-web';
+import {CallbackComponent} from './callback.component';
+
+const appRoutes: Routes = [
+  { path: 'callback', component: CallbackComponent },
+  { path: 'new-exam', component: ExamFormComponent },
+  { path: '', component: ExamsComponent },
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamFormComponent,
+    ExamsComponent,
+    CallbackComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+    ),
   ],
   providers: [ExamsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor() {
+    Auth0.configure({
+      domain: 'dev-bldw-h-w.auth0.com',
+      audience: 'https://apisimplify.com.br',
+      clientID: 'dAUM70DweYJySwmxruWVqx0RmjFwJdcg',
+      redirectUri: 'http://localhost:4200/callback',
+      scope: 'openid profile manage:exams'
+    });
+  }
 }
--- frontend/src/app/exams/exams-api.service.ts
+++ frontend/src/app/exams/exams-api.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import {API_URL} from '../env';
 import {Exam} from './exam.model';
+import * as Auth0 from 'auth0-web';
 
 @Injectable()
 export class ExamsApiService {
@@ -20,5 +21,10 @@ export class ExamsApiService {
     return this.http
       .get< Exam[] >(`${API_URL}/exams`)
       .catch(ExamsApiService._handleError);
+  }
+
+  saveExam(exam: Exam): Observable<any> {
+    return this.http
+      .post(`${API_URL}/exams`, exam);
   }
 }
