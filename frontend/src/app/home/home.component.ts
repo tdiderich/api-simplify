38a109b6db8522e866b1e6b282cb72c1136156fb
tdiderich <tyler.diderich@jask.io>
2019-06-21 13:23:28 -0500

    Form setup to create new api call in the database

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -11,4 +11,38 @@ import {Subscription} from 'rxjs/Subscription';
   selector: 'home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  apicall = {
+    name: '',
+    endpoint: '',
+    headers: '',
+    parameters: '',
+  };
+
+  constructor(private apiCallApi: ApiCallService, private router: Router) { }
+
+  updateName(event: any) {
+    this.apicall.name = event.target.value;
+  }
+
+  updateEndpoint(event: any) {
+    this.apicall.endpoint = event.target.value;
+  }
+
+  updateHeaders(event: any) {
+    this.apicall.headers = event.target.value;
+  }
+
+  updateParameters(event: any) {
+    this.apicall.parameters = event.target.value;
+  }
+
+  saveApiCalls() {
+    this.apiCallApi
+      .saveApiCalls(this.apicall)
+      .subscribe(
+        () => this.router.navigate(['/']),
+        error => alert(error.message)
+      );
+  }
+}
